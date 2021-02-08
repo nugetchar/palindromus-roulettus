2553767c0b80ec7888b7cb0862ebadd1a0e1c891
thomas.toledo <thomas.toledo@eemi.com>
2021-02-08 01:25:43 +0100

    update app name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/palindromeRoulette'),
+      dir: require('path').join(__dirname, './coverage/palindromus-roulettus'),
       subdir: '.',
       reporters: [
         { type: 'html' },
